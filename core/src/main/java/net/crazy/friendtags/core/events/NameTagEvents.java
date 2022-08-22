1449a543dd01b4e94bc6a5838f6be5e5cd632c2b
CrazySchnetzler1 <jan.web@outlook.de>
2022-08-22 13:51:42 +0200

    NameTagBackgroundRenderEvent

---
+++ core/src/main/java/net/crazy/friendtags/core/events/NameTagEvents.java
@@ -0,0 +1,25 @@
+package net.crazy.friendtags.core.events;
+
+import net.crazy.friendtags.core.FriendTagsAddon;
+import net.labymod.api.client.entity.player.tag.event.NameTagBackgroundRenderEvent;
+import net.labymod.api.event.Subscribe;
+import net.labymod.api.inject.LabyGuice;
+
+public class NameTagEvents {
+  private final FriendTagsAddon addon;
+
+  public NameTagEvents() {
+    this.addon = LabyGuice.getInstance(FriendTagsAddon.class);
+  }
+
+  @Subscribe
+  public void onRender(NameTagBackgroundRenderEvent event) {
+    if (!addon.configuration().enabled().get())
+      return;
+
+    if (!addon.configuration().tag().background())
+      return;
+
+    event.setCancelled(true);
+  }
+}

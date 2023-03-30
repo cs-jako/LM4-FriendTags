a7cb6e7eab0bab6cbdf7a9023af977e4ee0d7d18
Crazy <contact@csjako.de>
2023-03-30 16:13:05 +0200

    Bump LabyGradle & Addon version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "friendtags"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.7"
+    val labyGradlePluginVersion = "0.3.20"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }

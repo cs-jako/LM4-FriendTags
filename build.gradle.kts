d540314f19447f76cb4146ee93e620972b32be94
Jan K <52606324+cs-jako@users.noreply.github.com>
2024-01-08 15:43:51 +0100

    Develop (#6)
    * Updated to latest api version
    
    * Bump LabyGradle Version to latest, updated i18n file names
    
    * Also updated versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,11 +24,16 @@ labyMod {
     minecraft {
         registerVersions(
                 "1.8.9",
+                "1.12.2",
+                "1.16.5",
                 "1.17.1",
                 "1.18.2",
                 "1.19.2",
                 "1.19.3",
-                "23w04a"
+                "1.19.4",
+                "1.20.1",
+                "1.20.2",
+                "1.20.4"
         ) { version, provider ->
             configureRun(provider, version)
         }

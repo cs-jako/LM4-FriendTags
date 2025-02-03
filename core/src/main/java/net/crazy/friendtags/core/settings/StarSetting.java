b21cce188cce68bb4105822a9cc49aa962904463
Jan K <52606324+cs-jako@users.noreply.github.com>
2025-02-03 18:26:40 +0100

    Reconfigured tag registration

--- core/src/main/java/net/crazy/friendtags/core/settings/StarSetting.java
+++ core/src/main/java/net/crazy/friendtags/core/settings/StarSetting.java
@@ -5,7 +5,7 @@ import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.Switc
 import net.labymod.api.client.gui.screen.widget.widgets.input.color.ColorPickerWidget.ColorPickerSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.dropdown.DropdownWidget.DropdownSetting;
 import net.labymod.api.configuration.loader.Config;
-import net.labymod.api.configuration.loader.annotation.ParentSwitch;
+import net.labymod.api.configuration.loader.annotation.ShowSettingInParent;
 import net.labymod.api.configuration.loader.annotation.SpriteSlot;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 import net.labymod.api.configuration.settings.annotation.SettingSection;
@@ -14,7 +14,7 @@ import java.awt.*;
 
 public class StarSetting extends Config {
 
-  @ParentSwitch
+  @ShowSettingInParent
   @SwitchSetting
   private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 

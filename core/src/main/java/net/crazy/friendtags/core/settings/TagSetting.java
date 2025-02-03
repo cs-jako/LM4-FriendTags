e843712a881bfba89c6bdefe795013747f50f734
Jan K <52606324+cs-jako@users.noreply.github.com>
2025-02-03 18:36:21 +0100

    Removed deprecated ParentSwitch

--- core/src/main/java/net/crazy/friendtags/core/settings/TagSetting.java
+++ core/src/main/java/net/crazy/friendtags/core/settings/TagSetting.java
@@ -6,13 +6,14 @@ import net.labymod.api.client.gui.screen.widget.widgets.input.TextFieldWidget.Te
 import net.labymod.api.client.gui.screen.widget.widgets.input.dropdown.DropdownWidget.DropdownSetting;
 import net.labymod.api.configuration.loader.Config;
 import net.labymod.api.configuration.loader.annotation.ParentSwitch;
+import net.labymod.api.configuration.loader.annotation.ShowSettingInParent;
 import net.labymod.api.configuration.loader.annotation.SpriteSlot;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 import net.labymod.api.util.MethodOrder;
 
 public class TagSetting extends Config {
 
-  @ParentSwitch
+  @ShowSettingInParent
   @SwitchSetting
   private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
